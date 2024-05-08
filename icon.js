4a1c6ce2d440c8964794fe23f136722e1db35fe7
fontawsome <leinad111719@gmail.com>
2024-05-08 14:39:24 +0800

    Create icon.js

---
+++ icon.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var targetDate = new Date("2024-05-08T14:40:00");
+
+    if (new Date().getTime() >= targetDate.getTime()) {
+        alert("wow");
+        console.log("shesh");
+
+        var secondsLeft = 3;
+        var interval = setInterval(function() {
+            var minutes = Math.floor(secondsLeft / 60);
+            var seconds = secondsLeft % 60;
+            var formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+            console.log(minutes + ":" + formattedSeconds);
+            
+            if (--secondsLeft < 0) {
+                clearInterval(interval);
+                window.location.href = "https://www.youtube.com/watch?v=cvh0nX08nRw";
+            }
+        }, 1000);
+    } else {
+        console.log("Not the target date and time. Script will not run.");
+    }
+});
